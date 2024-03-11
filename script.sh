8f79fb0b9ce4090f99200210d59fa6bcfed91352
mehrankazemi1122 <35174945+mehrankazemi1122@users.noreply.github.com>
2024-03-12 00:32:08 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,13 +17,13 @@ fi
 # Iterate over each line in the file
 while IFS= read -r line; do
     response=$(curl -s "$line")
-    echo $response | base64
-    if [ -z "$response" ]; then
+#    echo $response | base64
+    if [ "$response" = $'\n' ]; then
         echo "nothing !!!"
     else
         # Convert the response to a JSON object with jq
         # The response is assumed to be a simple string that needs to be JSON encoded
-        json_response=$(jq -nc --arg resp "$response" '{"content": $resp}')
+        json_response=$(jq -nc --arg resp "$response" '{"content": $resp,"target": $line}')
 
         # Send the JSON response to test.com
         curl -X POST -H "Content-Type: application/json" -d "$json_response" https://discord.com/api/webhooks/1216775264588005407/KPWsewc6mEFSPesILV8WFDi8eB6SXC6wZ_Inno-_GJU_jQKzSXdp5NX3HAtIuNzZyUEX
