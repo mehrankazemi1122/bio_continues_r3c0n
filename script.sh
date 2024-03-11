1453fd460719fe77ff862922b74d36f0f3442966
mehrankazemi1122 <35174945+mehrankazemi1122@users.noreply.github.com>
2024-03-12 00:08:07 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,10 +16,16 @@ fi
 
 # Iterate over each line in the file
 while IFS= read -r line; do
-    response=$(curl -s "$line")
+    response=$(curl -s "$line" | tr -d '[:space:]')
     if [ -z "$response" ]; then
         echo "nothing !!!"
     else
-        curl -X POST -d "response : $response \n target: $line" https://discord.com/api/webhooks/1216775264588005407/KPWsewc6mEFSPesILV8WFDi8eB6SXC6wZ_Inno-_GJU_jQKzSXdp5NX3HAtIuNzZyUEX
+        # Ensure the response is not empty or just whitespace before sending
+        trimmed_response=$(echo "$response" | tr -d '[:space:]')
+        if [ -z "$trimmed_response" ]; then
+            echo "nothing !!!"
+        else
+            curl -X POST -d "$response" https://discord.com/api/webhooks/1216775264588005407/KPWsewc6mEFSPesILV8WFDi8eB6SXC6wZ_Inno-_GJU_jQKzSXdp5NX3HAtIuNzZyUEX
+        fi
     fi
 done < "$filename"
